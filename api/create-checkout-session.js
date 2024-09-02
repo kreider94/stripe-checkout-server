62e43b569013b9df1010cd937e045b2122fbd975
Kirstin Reid <kirstinreid94@gmail.com>
2024-09-02 17:14:43 +0100

    change node version

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -1,48 +1,42 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const cors = require('cors');
-const bodyParser = require('body-parser');
+import Cors from 'cors';
+import Stripe from 'stripe';
 
-const webDomain = process.env.WEB_DOMAIN || "";
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+const cors = Cors({
+  methods: ['GET', 'POST', 'OPTIONS'],
+});
 
-const handler = async (req, res) => {
+export default async function handler(req, res) {
+  await new Promise((resolve) => cors(req, res, resolve));
+  
   if (req.method === 'POST') {
-    cors()(req, res, async () => {
-      const { productName, customMetadata } = req.body; // Get dynamic data from the request body
+    const { productName, customMetadata } = req.body;
 
-      try {
-        const session = await stripe.checkout.sessions.create({
-          payment_method_types: ['card'],
-          line_items: [{
-            price_data: {
-              currency: 'usd',
-              product_data: {
-                name: productName,
-              },
-              unit_amount: 1000,
+    try {
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: [{
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: productName,
             },
-            quantity: 1,
-          }],
-          metadata: customMetadata,
-          mode: 'payment',
-          success_url: `https://${webDomain}/success`,
-          cancel_url: `https://${webDomain}/cancel`,
-        });
+            unit_amount: 1000, // Example amount (in cents)
+          },
+          quantity: 1,
+        }],
+        metadata: customMetadata,
+        mode: 'payment',
+        success_url: `https://${process.env.WEB_DOMAIN}/success`,
+        cancel_url: `https://${process.env.WEB_DOMAIN}/cancel`,
+      });
 
-        res.status(200).json({ id: session.id });
-      } catch (error) {
-        res.status(500).json({ error: error.message });
-      }
-    });
+      res.status(200).json({ id: session.id });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
   } else {
     res.setHeader('Allow', ['POST']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
-};
-
-export default handler;
-
-export const config = {
-  api: {
-    bodyParser: true,
-  },
-};
+}
