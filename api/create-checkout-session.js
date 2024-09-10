8f1a94cdce77cb11ca0da8b7f17211f747919634
Kirstin Reid <kirstinreid94@gmail.com>
2024-09-10 10:34:27 +0100

    add attribute fallbacks

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
   await new Promise((resolve) => cors(req, res, resolve));
   
   if (req.method === 'POST') {
-    const { productName, customMetadata } = req.body;
+    const { productName = "", customMetadata = "" } = req.body;
 
     try {
       const session = await stripe.checkout.sessions.create({
